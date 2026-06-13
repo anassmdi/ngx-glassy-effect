cb83d2560346d6a3bb26961482bff19f53b011e0
Anass <225123948+anassmdi@users.noreply.github.com>
2026-06-13 16:34:33 +0100

    init : glassy-effect-test

---
+++ projects/glassy-effect-test/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { GlassyEffect } from 'ngx-glassy-effect';
+
+@Component({
+  selector: 'app-root',
+  imports: [GlassyEffect],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+}
